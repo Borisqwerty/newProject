8b6bb257812e9abcb62c5664e18aecc17b7e6ed2
Boris-91 <freskoukraine@gmail.com>
2018-05-07 14:38:23 +0300

    initial_develop

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var app = express();
+var port = 3030;
+
+// var connection = mongoose.createConnection('mongodb://localhost:27017/testDb');
+mongoose.connect('mongodb://localhost:27017/testDb');
+var connection = mongoose.connection;
+
+connection.once('connected', function () {
+    console.log('-----connected to DB------');
+
+    app.use(bodyParser.json());
+
+    require('./routes/index')(app);
+
+    app.listen(port, function () {
+        console.log('server listening on port ' + port);
+    });
+});
+
+connection.on('error', function (err) {
+    console.log('Error', err);
+
+    process.exit(1);
+});
